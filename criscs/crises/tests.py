ce01551aaeb47c6fa4a67156345b3fabfbb6cfda
Brandon Fairchild <brandonfairchild26@gmail.com>
2013-07-10 21:03:30 -0500

    More tests.

--- criscs/crises/tests.py
+++ criscs/crises/tests.py
@@ -8,7 +8,7 @@ Replace this with more appropriate tests for your application.
 import unittest
 import xml.etree.ElementTree as ET
 from ModelFactory import create_crisis_element, create_org_element, create_person_element, read_wcdb_model, read_list_content
-from XMLUtility import print_rec_xml, read_xml, initialize_pages, parse_models, parse_xml, process_xml
+from XMLUtility import print_rec_xml, initialize_pages, parse_models, parse_xml, process_xml, read_and_validate_xml
 from crises.models import WCDBElement, Crisis, Organization, Person, ListType, LI, R_Crisis_Person, R_Crisis_Org, R_Org_Person
 from genxmlif import GenXmlIfError
 from minixsv import pyxsval
@@ -41,6 +41,116 @@ class TestXMLUtility (unittest.TestCase) :
 		try :
 			testXMLFile = "crises/test_data/WorldCrises_bad_root.xml"
 			model = process_xml(testXMLFile)	
+			assert(False)
+		except :
+			assert(True)
+
+	def test_process_xml_3(self) :
+		try :
+			testXMLFile = ""
+			model = process_xml(testXMLFile)	
+			assert(False)
+		except :
+			assert(True)
+
+	def test_initialize_pages_1(self) :
+		all_models = initialize_pages()
+		assert(len(all_models) == 5)
+		assert("crises" in all_models)
+		assert("list_types" in all_models)
+		assert("orgs" in all_models)
+		assert("list_types" in all_models)
+		assert("list_elements" in all_models)
+
+	def test_read_and_validate_xml_1(self) :
+		testXMLFile = "crises/test_data/WorldCrises_good.xml"
+		model = read_and_validate_xml(testXMLFile)
+		assert (model is not None)
+
+	def test_read_and_validate_xml_2(self) :
+		testXMLFile = "crises/test_data/WorldCrises_bad_root.xml"
+		try :
+			model = read_and_validate_xml(testXMLFile)
+			assert(False)
+		except :
+			assert(True)
+
+	def test_read_and_validate_xml_3(self) :
+		testXMLFile = "crises/test_data/WorldCrises_bad_id.xml"
+		try :
+			model = read_and_validate_xml(testXMLFile)
+			assert(False)
+		except :
+			assert(True)	
+
+	def test_parse_xml_1(self) :
+		testXMLFile = "crises/test_data/WorldCrises_good.xml"
+		xml = read_and_validate_xml(testXMLFile)
+		model = parse_xml (xml)
+		assert(len(model) == 5)
+		assert(len(model["crises"])==1)
+		assert(type(model["crises"]["CRI_NRINFL"]) is Crisis)
+		assert(len(model["people"])==1)
+		assert(type(model["people"]["PER_MNSNGH"]) is Person)
+		assert(len(model["orgs"])==1)
+		assert(type(model["orgs"]["ORG_PMRLFD"]) is Organization)
+
+	def test_parse_xml_2(self) :
+		try :
+			testXMLFile = "crises/test_data/WorldCrises_bad_root.xml"
+			xml = read_and_validate_xml(testXMLFile)
+			model = parse_xml (xml)	
+			assert(False)
+		except :
+			assert(True)
+
+	def test_parse_xml_3(self) :
+		try :
+			testXMLFile = ""
+			xml = read_and_validate_xml(testXMLFile)
+			model = parse_xml (xml)		
+			assert(False)
+		except :
+			assert(True)
+
+	def test_print_rec_xml_1(self) :
+		try :
+			testXMLFile = "crises/test_data/WorldCrises_good.xml"
+			xml = read_and_validate_xml(testXMLFile)
+			print_rec_xml(xml, 0)
+			assert(True)
+		except :
+			assert(False)
+
+	def test_parse_models_1(self) :
+		testXMLFile = "crises/test_data/WorldCrises_good.xml"
+		root = read_and_validate_xml (testXMLFile)
+		model = initialize_pages ()
+		parse_models (root, model)
+		assert(len(model) == 5)
+		assert(len(model["crises"])==1)
+		assert(type(model["crises"]["CRI_NRINFL"]) is Crisis)
+		assert(len(model["people"])==1)
+		assert(type(model["people"]["PER_MNSNGH"]) is Person)
+		assert(len(model["orgs"])==1)
+		assert(type(model["orgs"]["ORG_PMRLFD"]) is Organization)
+
+	def test_parse_models_2(self) :
+		try :
+			testXMLFile = "crises/test_data/WorldCrises_good.xml"
+			root = read_and_validate_xml (testXMLFile)
+			model = initialize_pages ()
+			parse_models (root, model)
+			assert(False)
+		except :
+			assert(True)
+
+	def test_parse_models_3(self) :
+		try :
+			testXMLFile = "crises/test_data/WorldCrises_good.xml"
+			root = read_and_validate_xml (testXMLFile)
+			model = initialize_pages ()
+			parse_models (root, model)	
 			assert(False)
 		except :
 			assert(True)
