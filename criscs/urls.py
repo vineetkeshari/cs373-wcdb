697d5dfa98f2599240503a7ca5fe5ea34c93050f
Vineet Keshari <vkeshari@cs.utexas.edu>
2013-08-05 21:01:04 -0500

    Fixed urls.py

--- criscs/urls.py
+++ criscs/urls.py
@@ -12,11 +12,8 @@ urlpatterns = patterns('',
     url(r'^crises/$', 'crises.views.index'),
     url(r'^crises/(?P<view_id>\w{10})/$', 'crises.views.base_view'),
     url(r'^import/$', 'crises.views.import_file', name='import_file'),
-<<<<<<< HEAD
     url(r'^merge_import/$', 'crises.views.merge_import_file', name='merge_import_file'),
-=======
     url(r'^search_results.html/$', 'crises.views.search_results', name='search_results'),
->>>>>>> b7ea218a05ce69dee2fcfe5adcaa224bc1333e82
     url(r'^export/$', 'crises.views.export_file', name='export_file'),
     url(r'^unittests/$', 'crises.views.run_tests', name='run_tests'),
 
